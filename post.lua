6b74f4f033175cb76e6f6001d79f477b2fc9d446
jt232003 <21f2000730@ds.study.iitm.ac.in>
2025-08-24 13:36:43 +0000

    feat: Add Kubernetes deployment, service, and HPA manifests

---
+++ post.lua
@@ -0,0 +1,3 @@
+wrk.method = "POST"
+wrk.body   = '{"age": 58, "gender": 0, "cp": 0, "trestbps": 100, "chol": 248, "fbs": 0, "restecg": 0, "thalach": 122, "exang": 0, "oldpeak": 1, "slope": 1, "ca": 0, "thal": 2}'
+wrk.headers["Content-Type"] = "application/json"
